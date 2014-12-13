ee1daf2c8f818d882e8a2f43d87ef8c6a5a3c618
Korneliusz Caputa <helluinster@gmail.com>
2014-12-13 20:00:50 +0100

    Externalize email for gravatar src.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,11 +3,8 @@
 angular.module("interactiveCvApp")
     .controller("MainCtrl", function ($scope, $rootScope, $http) {
 
-        $http.get("/api/awesomeThings").success(function (awesomeThings) {
-            $scope.awesomeThings = awesomeThings;
-        });
-
         $http.get("/api/appInfo").success(function (appInfo) {
             $scope.appInfo = appInfo;
+            console.log(appInfo);
         });
     });
